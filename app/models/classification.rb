027b56afb7795a6bb6802bb32beb6eec89fb9817
AmSmo <adam.smolenski@gmail.com>
2020-09-06 15:23:24 -0400

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,13 +1,22 @@
+# == Schema Information
+#
+# Table name: classifications
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
 
   def self.my_all
-    # all
+    self.all
   end
 
   def self.longest
-    # Boat.longest.classifications
+    Boat.longest.classifications
   end
 
 end
--- spec/models/classification_spec.rb
+++ spec/models/classification_spec.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: classifications
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'spec_helper'
 
 describe Classification do
